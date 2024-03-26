74ade1db53d2c4307a0e6e19bc0d988b53d8af10
Fasil <fasilvaliyattil843@gmail.com>
2024-03-26 20:54:02 +0530

    fetching continent data added

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-fetch('https://countries.trevorblades.com/')
+const continentSelect = document.getElementById('continent-select')
+const continentList = document.getElementById('continent-list')
+
+fetch('https://countries.trevorblades.com/', {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+        query: `
+            query {
+                continents {
+                    name, code
+                }
+            }
+        `
+    })
+}).then(res => res.json())
+.then(data => {
+    console.log(data);
+})
