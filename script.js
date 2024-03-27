0383d5c5ff8413ea3e126f1a14787ff941d9494f
Fasil <fasilvaliyattil843@gmail.com>
2024-03-27 09:53:21 +0530

    contries list completed

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
 const continentSelect = document.getElementById('continent-select')
-const continentList = document.getElementById('continent-list')
+const countryList = document.getElementById('countries-list')
 
-fetch('https://countries.trevorblades.com/', {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-        query: `
-            query {
-                continents {
-                    name, code
+queryFetch(`
+query {
+    continents {
+        name, code
+    }
+}
+`, { code: "" }).then(data => {
+    console.log(data, 'data');
+    data.data.continents.forEach(continent => {
+        const option = document.createElement('option')
+        option.value = continent.code
+        option.innerText = continent.name
+        continentSelect.append(option)
+    })
+})
+
+continentSelect.addEventListener('change', (e) => {
+    console.log('event listener invoked');
+    const continentCode = e.target.value
+    getCountries(continentCode).then((countries) => {
+        console.log(countries, 'countries');
+        countryList.innerHTML = ''
+        countries.forEach(country => {
+            const element = document.createElement('div')
+            element.innerText = country.name
+            countryList.append(element)
+        })
+    })
+})
+
+async function getCountries(continentCode) {
+    return queryFetch(`
+        query getCountries($code: ID!) {
+            continent(code: $code) {
+                countries {
+                    name
                 }
             }
-        `
-    })
-}).then(res => res.json())
-.then(data => {
-    console.log(data);
-})
+        }
+    
+    `, { code: continentCode })
+    .then(data => data.data.continent.countries)
+}
+
+async function queryFetch(query, variables) {
+    return await fetch('https://countries.trevorblades.com/', {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ query, variables })
+    }).then(res => res.json())
+}
